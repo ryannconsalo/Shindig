e2b67ec469486373f8156f8337c9f4fec4f1912b
Ryann <rjc6ez@virginia.edu>
2017-06-20 13:56:38 +0200

    Shindig Class creation

---
+++ Shindig/Shindig.swift
@@ -0,0 +1,36 @@
+//
+//  Shindig.swift
+//  Shindig
+//
+//  Created by Ryann Consalo on 2017/06/20.
+//  Copyright © 2017 Ryann Consalo. All rights reserved.
+//
+
+import Foundation
+
+
+class Shindig {
+    
+    var host: String?
+    var key : String?
+    var date : String?
+    var totalPool : Int?
+    var invited = [String]()
+    var coming = [String]()
+    var location : String?
+    var price : Int?
+    var supplies = [String]()
+    var time : String?
+    
+    init? (host: String?, date: String?, location: String?, price: Int?, time: String?) {
+        self.host = host
+        self.date = date
+        self.location = location
+        self.price = price
+        self.time = time
+        self.totalPool = 0.00
+    }
+    
+    
+    
+}
