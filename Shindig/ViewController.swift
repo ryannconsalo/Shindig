9973db5ba5e7bb8496e22b1e6873ccb88ea759e2
Ryann <rjc6ez@virginia.edu>
2017-06-20 11:28:54 +0200

    Initial Commit

---
+++ Shindig/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shindig
+//
+//  Created by Ryann Consalo on 2017/06/20.
+//  Copyright © 2017 Ryann Consalo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
